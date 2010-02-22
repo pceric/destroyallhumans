1b55f16498e3d3f2c39a8e8a05b7eae86edc97d3
hokanson <hokanson@51cfe5e2-f81a-11de-a7af-57dd9f136cd4>
2010-02-22 01:16:33 +0000

    Add controller code.

---
+++ trunk/Arduino/robot/Controller.h
@@ -0,0 +1,28 @@
+// Controller Dead Zone
+#define DEAD_ZONE 10
+
+typedef struct {
+  long timestamp;
+  
+  uint8_t X;
+  uint8_t C;
+  uint8_t T; 
+  uint8_t S;
+  uint8_t L1;
+  uint8_t L2;
+  uint8_t L3;
+  uint8_t R1;
+  uint8_t R2;
+  uint8_t R3;
+  uint8_t Select;
+  uint8_t Start;
+  uint8_t Up;
+  uint8_t Down;
+  uint8_t Left;
+  uint8_t Right;
+  
+  char LeftX;
+  char LeftY;
+  char RightX;
+  char RightY;
+} Controller;
