ffee7da589aa6adfc760084f525a1065ad178354
hokanson <hokanson@51cfe5e2-f81a-11de-a7af-57dd9f136cd4>
2010-04-16 05:06:54 +0000

    Add offset and targeting config.

---
+++ trunk/Processing/RoboCommand/TargetBlob.java
@@ -0,0 +1,33 @@
+/*
+ * This is a Singlton class to store the state of the robot. 
+ * 
+ */
+public class TargetBlob
+{
+    
+    public long timestamp;  
+  
+    public int luma = 0;
+    
+    public int chromaRed = 0;
+
+    public int chromaBlue = 0;
+     
+    public int x = 0;
+    
+    public int y = 0;
+    
+    //public int z = 0;
+    
+    public int width = 0;
+    
+    public int height = 0;
+    
+    
+    public String toString()
+    {
+      return "Y:" + luma + " U:" + chromaBlue + " V:" + chromaRed + " X:" + x + " Y:" + y + " W:" + width + " H:" + height;   
+    }
+    
+    
+}
