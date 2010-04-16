5ab1ce18ac8c7381fdc58d2469cd51d127e122bd
darrellt <darrellt@51cfe5e2-f81a-11de-a7af-57dd9f136cd4>
2010-04-16 04:38:48 +0000

    Add targetSettings to processing project

---
+++ trunk/Processing/RoboCommand/TargetSettings.java
@@ -0,0 +1,32 @@
+//package com.atg.netcat;
+
+/*
+ * This is a Singlton class to store the state of the robot. 
+ * 
+ */
+public class TargetSettings
+{
+    
+    public long timestamp;  
+  
+    public int targetLuma = 0;
+    
+    public int targetChromaBlue = 122;
+    
+    public int targetChromaRed = 24;
+     
+    //public int redBalance = -128;
+     
+    ///public int blueBalance = -128;
+    
+    public int tolerance = 24;
+    
+    public int leftCrossHairX = 256;
+    
+    public int leftCrossHairY = 256;   
+    
+    public int rightCrossHairX = 256;
+    
+    public int rightCrossHairY = 256;
+    
+}
