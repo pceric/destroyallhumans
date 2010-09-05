c05835c4f749037daea7729cd62f924df6e7be86
hokanson <hokanson@gmail.com>
2010-09-05 23:26:32 +0000

    Add new ServoBot project.

---
+++ JAVA/ServoBot/src/com/allthingsgeek/servobot/IPCommThread.java
@@ -0,0 +1,177 @@
+/*
+ * Copyright 2010 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License
+ */
+
+package com.allthingsgeek.servobot;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+import android.app.ProgressDialog;
+
+import android.os.Handler;
+import android.util.Log;
+
+class IPCommThread extends Thread {
+
+	protected String tostText = "";
+
+	protected String logText = "";
+
+	byte[] buffer = new byte[256];
+
+	ServerSocket serverSocket = null;
+
+	InputStream iStream = null;
+
+	OutputStream oStream = null;
+
+	Socket socket = null;
+
+	boolean stopListening = false;
+
+	private Integer listenPort;
+
+	private int sleepTime = 200;
+
+	public static InetAddress clientAddress;
+
+	private ProgressDialog dialog;
+
+	RobotState state;
+
+	private Movement mover;
+
+	public IPCommThread(Integer port, ProgressDialog dialog, RobotState state) {
+		this.dialog = dialog;
+		this.listenPort = port;
+		this.state = state;
+		this.mover = Movement.getInstance();
+	}
+
+	public void run() {
+		int readlen = 0;
+
+		while (!stopListening) {
+
+			if (serverSocket == null) {
+				try {
+					serverSocket = new ServerSocket(listenPort);
+					// logText += "Listening on port " + listenPort +
+					// " for Connections\n";
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} else if (socket == null) {
+				try {
+					// tostText = "Connecting";
+					// logText += "Connecting...\n";
+					// handler.sendEmptyMessage(0);
+					socket = serverSocket.accept();
+				} catch (IOException e) {
+					// tv.appendi"Socket: " + e.getMessage());
+					// e.printStackTrace();
+				}
+
+			} else if (iStream == null) {
+				try {
+
+					iStream = new DataInputStream(socket.getInputStream());
+					oStream = new DataOutputStream(socket.getOutputStream());
+
+					clientAddress = socket.getInetAddress();
+					if (dialog != null && dialog.isShowing())
+						dialog.dismiss();
+				} catch (IOException e) {
+					// tv.appendi"Socket: " + e.getMessage());
+					// e.printStackTrace();
+				}
+
+			} else {
+				try {
+					if (serverSocket.isClosed()) {
+						iStream = null;
+						oStream = null;
+						socket = null;
+						// tostText = "Disconnected";
+						// logText += "Disconnected!\n";
+					} else {
+						readlen = iStream.read(buffer, 0, 100);
+						if (readlen > 0) {
+							mover.processTextCommand(new String(buffer, 0,
+									readlen - 1));
+						}
+					}
+
+				} catch (IOException e) {
+					// tv.append(e.getMessage());
+					return;
+				}
+			}
+			try {
+				Thread.sleep(sleepTime);
+			} catch (InterruptedException e) {
+
+				// tv.append(e.getMessage());
+				// e.printStackTrace();
+			}
+		}
+
+		try {
+			if (serverSocket != null)
+				;
+			{
+				serverSocket.close();
+			}
+
+		} catch (Exception e1) {
+		}
+		socket = null;
+		iStream = null;
+		oStream = null;
+		serverSocket = null;
+		stopListening = false;
+	}
+
+	/* Call this from the main Activity to send data to the remote device */
+	public void write(byte[] bytes) {
+		try {
+			if (oStream != null) {
+				oStream.write(bytes);
+			}
+		} catch (IOException e) {
+			Log.e("CommThread.write", "exception during write", e);
+		}
+	}
+
+	public boolean isConnected() {
+		return oStream != null;
+	}
+
+	/* Call this from the main Activity to shutdown the connection */
+	public void cancel() {
+
+		stopListening = true;
+
+	}
+}
---
+++ JAVA/ServoBot/src/com/allthingsgeek/servobot/SetupActivity.java
@@ -0,0 +1,172 @@
+/*
+ * Robot control console. Copyright (C) 2010 Darrell Taylor & Eric Hokanson
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.allthingsgeek.servobot;
+
+import com.allthingsgeek.servobot.R;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+/**
+ * @author ehokanso
+ * 
+ */
+public class SetupActivity extends Activity implements OnSeekBarChangeListener {
+	public static SensorManager sensorManager;
+	
+	SeekBar lPulseBar;
+	SeekBar lPulseBar2;
+	SeekBar rPulseBar;
+	SeekBar rPulseBar2;
+	SeekBar lrOffset;
+	TextView rPulseText;
+	TextView rPulseText2;
+	TextView lPulseText;
+	TextView lPulseText2;
+	TextView lrOffsetText;
+	ToggleButton soundToggleButton;
+	PulseGenerator noise;
+	Thread noiseThread;
+	GestureDetector nGestures;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.setup);
+
+		// set up the noise thread
+	    noise = PulseGenerator.getInstance();
+	    
+		lPulseBar = (SeekBar) findViewById(R.id.LeftServo);
+		lPulseBar.setProgress((noise.getPulsePercent(0) - 40) * 5);
+		lPulseBar.setOnSeekBarChangeListener(this);
+		lPulseText = (TextView) findViewById(R.id.LeftServoValue);
+		lPulseText.setText("Servo 1(L Wheel) = " + noise.getPulsePercent(0)
+				+ "% (" + noise.getPulseSamples(0) + " samples)");
+
+	    lPulseBar2 = (SeekBar) findViewById(R.id.LeftServo2);
+		lPulseBar2.setProgress((noise.getPulsePercent(1) - 40) * 5);
+	    lPulseBar2.setOnSeekBarChangeListener(this);
+	    lPulseText2 = (TextView) findViewById(R.id.LeftServoValue2);
+		lPulseText2.setText("Servo 2(L Arm) = " + noise.getPulsePercent(1)
+				+ "% (" + noise.getPulseSamples(1) + " samples)");
+	    
+		rPulseBar = (SeekBar) findViewById(R.id.RightServo);
+		rPulseBar.setProgress((noise.getPulsePercent(2) - 40) * 5);
+		rPulseBar.setOnSeekBarChangeListener(this);
+		rPulseText = (TextView) findViewById(R.id.RightServoValue);
+		rPulseText.setText("Servo 3(R Wheel) = " + noise.getPulsePercent(2)
+				+ "% (" + noise.getPulseSamples(2) + " samples)");
+
+	    rPulseBar2 = (SeekBar) findViewById(R.id.RightServo2);
+		rPulseBar2.setProgress((noise.getPulsePercent(3) - 40) * 5);
+	    rPulseBar2.setOnSeekBarChangeListener(this);
+	    rPulseText2 = (TextView) findViewById(R.id.RightServoValue2);
+		rPulseText2.setText("Servo 4(R Arm) = " + noise.getPulsePercent(3)
+				+ "% (" + noise.getPulseSamples(3) + " samples)");
+		
+	    lrOffset = (SeekBar) findViewById(R.id.LROffset);
+	    lrOffset.setProgress((noise.getPulsePercent(3) - 40) * 5);
+	    lrOffset.setOnSeekBarChangeListener(this);
+	    lrOffsetText = (TextView) findViewById(R.id.RightServoValue2);
+	    lrOffsetText.setText("Left vs Right Offset = " + "");
+
+		soundToggleButton = (ToggleButton) findViewById(R.id.ToggleSound);
+		soundToggleButton.setChecked(!noise.isPaused());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onStop()
+	 */
+	@Override
+	protected void onStop() {
+		super.onStop();
+		SharedPreferences settings = getSharedPreferences(MainActivity.PREFS_NAME, 0);
+		SharedPreferences.Editor editor = settings.edit();
+		editor.putInt("servo1Percent", noise.getPulsePercent(0));
+		editor.putInt("servo2Percent", noise.getPulsePercent(1));
+		editor.putInt("servo3Percent", noise.getPulsePercent(2));
+		editor.putInt("servo4Percent", noise.getPulsePercent(3));
+		editor.putInt("wheelOffset", noise.getPulsePercent(3));
+		// Commit the edits!
+		editor.commit();
+		finish();
+	}
+
+	public void onToggleSound(View v) {
+    	ToggleButton t = (ToggleButton)v;
+    	if (t.isChecked())
+    		noise.pause(false);
+    	else
+    		noise.pause(true);
+	}
+
+	public void onProgressChanged(SeekBar seekBar, int progress,
+			boolean fromTouch) {
+		if (seekBar.getId() == lPulseBar.getId()) {
+			noise.setOffsetPulsePercent(progress / 2 + 25, 0);
+			lPulseText.setText("Servo 1(L Wheel) = " + noise.getPulsePercent(0)
+					+ "% (" + noise.getPulseSamples(0) + " samples)");
+		}
+		if (seekBar.getId() == lPulseBar2.getId()) {
+			noise.setOffsetPulsePercent(progress / 2 + 25, 1);
+			lPulseText2.setText("Servo 2(L Arm) = " + noise.getPulsePercent(1)
+					+ "% (" + noise.getPulseSamples(1) + " samples)");
+		}
+		if (seekBar.getId() == rPulseBar.getId()) {
+			noise.setOffsetPulsePercent(progress / 2 + 25, 2);
+			rPulseText.setText("Servo 3(R Wheel) = " + noise.getPulsePercent(2)
+					+ "% (" + noise.getPulseSamples(2) + " samples)");
+		}
+		if (seekBar.getId() == rPulseBar2.getId()) {
+			noise.setOffsetPulsePercent(progress / 2 + 25, 3);
+			rPulseText2.setText("Servo 4(R Arm) = " + noise.getPulsePercent(3)
+					+ "% (" + noise.getPulseSamples(3) + " samples)");
+		}
+		if (seekBar.getId() == lrOffset.getId()) {
+			//noise.setOffsetPulsePercent(progress / 2 + 25, 3);
+			lrOffsetText.setText("Left vs Right Offset = " + "");
+		}
+	}
+
+	public void onStartTrackingTouch(SeekBar seekBar) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void onStopTrackingTouch(SeekBar seekBar) {
+		// TODO Auto-generated method stub
+
+	}
+}
